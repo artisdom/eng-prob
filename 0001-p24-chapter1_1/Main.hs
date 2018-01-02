8d8f8b777804229b1c01a40f0816a9eb07002054
Richard Cook <rcook@rcook.org>
2018-01-02 08:27:48 -0800

    Distance between two points

---
+++ 0001-p24-chapter1_1/Main.hs
@@ -0,0 +1,27 @@
+{-|
+Module      : Main
+Description : 0001-chapter1_1
+Copyright   : (C) Richard Cook, 2018
+License     : MIT
+Maintainer  : rcook@rcook.org
+Stability   : experimental
+Portability : portable
+
+This program computes the distance between two points.
+-}
+
+module Main (main) where
+
+import Text.Printf (printf)
+
+main :: IO ()
+main = do
+    let x1 = 1
+        y1 = 5
+        x2 = 4
+        y2 = 7
+        side_1 = x2 - x1
+        side_2 = y2 - y1
+        distance = sqrt(side_1 ** 2 + side_2 ** 2)
+
+    putStrLn $ printf "The distance between the two points is %5.2f" (distance :: Double)
