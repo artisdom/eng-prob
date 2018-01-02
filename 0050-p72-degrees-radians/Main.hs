441e962278cb6da2795a1781e6ff62dbacc5c5df
Richard Cook <rcook@rcook.org>
2018-01-02 12:06:32 -0800

    More degree/radian conversions

---
+++ 0050-p72-degrees-radians/Main.hs
@@ -0,0 +1,43 @@
+{-|
+Module      : Main
+Description : 0050-p72-degrees-radians
+Copyright   : (C) Richard Cook, 2018
+License     : MIT
+Maintainer  : rcook@rcook.org
+Stability   : experimental
+Portability : portable
+
+This program demonstrates reusable degree/radian conversion programs.
+-}
+
+module Main (main) where
+
+import Text.Printf (printf)
+
+radiansToDegrees :: (Floating a, Fractional a) => a -> a
+--radiansToDegrees aRad = aRad * 180 / pi
+radiansToDegrees = (/ pi) . (180 *)
+
+degreesToRadians :: (Floating a, Fractional a) => a -> a
+--degreesToRadians aDeg = aDeg * pi / 180
+degreesToRadians = (/ 180) . (pi *)
+
+showRadiansToDegrees :: Double -> IO ()
+showRadiansToDegrees aRad = putStrLn $ printf "%g radians = %g degrees" aRad (radiansToDegrees aRad)
+
+showDegreesToRadians :: Double -> IO ()
+showDegreesToRadians aDeg = putStrLn $ printf "%g degrees = %g radians" aDeg (degreesToRadians aDeg)
+
+main :: IO ()
+main = do
+    showRadiansToDegrees 0
+    showRadiansToDegrees $ pi / 2
+    showRadiansToDegrees pi
+    showRadiansToDegrees $ 3 * pi / 2
+    showRadiansToDegrees $ 2 * pi
+
+    showDegreesToRadians 0
+    showDegreesToRadians 90
+    showDegreesToRadians 180
+    showDegreesToRadians 270
+    showDegreesToRadians 360
