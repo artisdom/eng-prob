99968cef3ccd90fc6d205dd7d9462d4bbf0fe572
Richard Cook <rcook@rcook.org>
2018-01-02 13:54:42 -0800

    Direct iteration over range with forM_ only

---
+++ 0100-p107-chapter3_2/Main.hs
@@ -0,0 +1,25 @@
+{-|
+Module      : Main
+Description : 0100-p107-chapter3_2
+Copyright   : (C) Richard Cook, 2018
+License     : MIT
+Maintainer  : rcook@rcook.org
+Stability   : experimental
+Portability : portable
+
+This programs prints a degree-to-radian table using @forM_@ only and @do@-notation.
+-}
+
+module Main (main) where
+
+import Control.Monad (forM_)
+import Text.Printf (printf)
+
+main :: IO ()
+main = do
+    -- Print degrees and radians over a range
+    putStrLn "Degrees to Radians"
+    forM_ [0 :: Int, 10..360] $ \d -> do
+        let degrees = fromIntegral d
+            radians = degrees * pi / 180
+        putStrLn $ printf "%6f %9.6f" (degrees :: Double) radians
