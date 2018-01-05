4698b4ccc2ad8efffe2b38a066b3151b8ad1b2ba
Richard Cook <rcook@rcook.org>
2018-01-05 11:33:48 -0800

    Generate weather balloon data file and chart

--- 0150-p129-chapter3_8/Main.hs
+++ 0150-p129-chapter3_8/Main.hs
@@ -7,36 +7,22 @@ Maintainer  : rcook@rcook.org
 Stability   : experimental
 Portability : portable
 
-This program generates a file of height and velocity values for a weather balloon. The information is also printed in a report.
+This program generates a file of height and velocity values for a weather balloon. The information is also printed in a report and output to a PNG file.
 -}
-
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
 import           Control.Monad (forM_)
-import           Data.Attoparsec.Text
-                    ( Parser
-                    , choice
-                    , double
-                    , endOfInput
-                    , skipSpace
-                    , manyTill'
+import           EngProb (prompt)
+import           Graphics.Rendering.Chart.Easy
+                    ( (.=)
+                    , def
+                    , layoutlr_title
+                    , line
+                    , plotLeft
+                    , plotRight
                     )
-import           Data.List (foldl')
-import           Data.Text (Text)
-import qualified Data.Text.IO as Text
-                    ( hPutStrLn
-                    , readFile
-                    )
-import           EngProb
-                    ( parseAll
-                    , prompt
-                    , skipSpace1
-                    )
-import           Paths_eng_prob (getDataFileName)
+import           Graphics.Rendering.Chart.Backend.Cairo (toFile)
 import           System.IO
                     ( IOMode(..)
                     , hPutStrLn
@@ -44,31 +30,22 @@ import           System.IO
                     )
 import           Text.Printf (printf)
 
--- Data type representing a (time, motion) pair from the data file
-data TimeMotion = TimeMotion Double Double deriving Show
+type DataPoint = (Double, Double, Double)
 
--- A parser for (time, motion) pairs
-timeMotion :: Parser TimeMotion
-timeMotion = TimeMotion <$> double <* skipSpace1 <*> double
-
-untilTrailerLine :: Parser [TimeMotion]
-untilTrailerLine = choice [none, oneOrMore]
-    where
-        none = skipSpace *> endOfInput *> pure []
-        oneOrMore = do
-            value <- timeMotion
-            values <- manyTill' (skipSpace1 *> timeMotion) none
-            return $ value : values
-
-parseDataText :: Text -> Either String [TimeMotion]
-parseDataText = parseAll untilTrailerLine
+createPlot :: FilePath -> [DataPoint] -> IO ()
+createPlot fileName dataPoints = toFile def fileName $ do
+    layoutlr_title .= "Weather balloon altitude and velocity"
+    plotLeft $ line "Balloon Altitude (metres)"
+                [ [ (d, h) | (d, h, _) <- dataPoints] ]
+    plotRight $ line "Balloon Velocity (metres/s)"
+                [ [ (d, v) | (d, _, v) <- dataPoints ] ]
 
 main :: IO ()
 main = do
     -- Get user input
-    initial :: Double <- prompt "Enter initial value for table (in hours)\n"
-    increment :: Double <- prompt "Enter increment between lines (in hours)\n"
-    final :: Double <- prompt "Enter final value for table (in hours)\n"
+    initial <- prompt "Enter initial value for table (in hours)\n"
+    increment <- prompt "Enter increment between lines (in hours)\n"
+    final <- prompt "Enter final value for table (in hours)\n"
 
     -- Print report heading
     putStrLn "\n\nWeather Balloon Information"
@@ -90,9 +67,9 @@ main = do
                     in (mt', mh', (time, height, velocity) : items))
                     (0, 0, [])
                     [initial, initial + increment .. final]
-        (maxTime :: Double, maxHeight :: Double, dataPoints) = result
+        (maxTime, maxHeight, dataPoints) = result
 
-    withFile "../balloon.dat" WriteMode $ \h ->
+    withFile "balloon.dat" WriteMode $ \h ->
         forM_ dataPoints $ \(time, height, velocity) -> do
             putStrLn $ printf "%6.2f  %8.2f   %7.2f" time height (velocity / 3600)
             hPutStrLn h $ printf "%.2f %.2f %.2f" time height (velocity / 3600)
@@ -100,3 +77,5 @@ main = do
     -- Print maximum height and corresponding time
     putStrLn $ printf "\nMaximum balloon height was %8.2f metres" maxHeight
     putStrLn $ printf "and it occurred at %6.2f hours" maxTime
+
+    createPlot "../balloon.png" dataPoints
