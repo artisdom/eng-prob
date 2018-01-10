12587a3e50e7cc24b787b9538dbbb50ab415f093
Richard Cook <rcook@rcook.org>
2018-01-10 10:19:56 -0800

    Scratch

--- scratch/Main.hs
+++ scratch/Main.hs
@@ -1,4 +1,19 @@
 module Main (main) where
 
+import           Control.Monad.State
+import           Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as Vector
+import           EngProb
+import           Paths_eng_prob (getDataFileName)
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+    fileName <- getDataFileName "sensor3.dat"
+    stream <- readFile fileName
+    let result = (flip Vector.unfoldr) (tokenize stream) $ \ts ->
+                    case parse ((,) <$> double <*> double) ts of
+                        Valid pair ts' -> Just (pair, ts')
+                        _ -> Nothing
+    print result
+    putStrLn "Done"
+
